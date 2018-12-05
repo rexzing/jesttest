91c15c59762c07ea1e7508e2d1c1a6c76b9a23e9
EMBLA\chathuranga <chaturanga@vetserve.no>
2018-12-05 14:48:02 +0530

    initial commit

---
+++ test/login.spec.js
@@ -0,0 +1,44 @@
+import puppeteer from "puppeteer";
+const APP = "http://eval-development-eval.10.10.1.183.xip.io";
+
+const lead = {
+  username: "766458467",
+  Password: "wolf5",
+  countrycode: "sr" 
+};
+
+let page;
+let browser;
+const width = 1920;
+const height = 1080;
+
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: [`--window-size=${width},${height}`]
+  });
+  page = await browser.newPage();
+  await page.setViewport({ width, height });
+});
+
+describe("Contact form", () => {
+  test("lead can submit a contact request", async () => {
+    await page.goto(APP);
+    await page.waitForSelector("#Username");
+    await page.click("input[name=CountryCode]");
+    await page.type("input[name=CountryCode]", lead.countrycode);
+    await page.click("input[name=Username]");
+    await page.type("input[name=Username]", lead.username);
+    await page.click("input[name=Password]");
+    await page.type("input[name=Password]", lead.Password);
+    await page.click("button[value=login]");
+    await page.waitFor(10000);
+    //await page.waitForSelector(".modal");
+  }, 32000);
+});
+
+afterAll(() => {
+  browser.close();
+});
