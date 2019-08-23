dbc4bbb419cc003034b96afb5c43d8ffd6ae8f94
Chamath Sandaru <ChamathS@embla.lk>
2019-08-23 17:17:37 +0530

    User login  process automation with auth dev server

--- test/login.spec.js
+++ test/login.spec.js
@@ -3,20 +3,24 @@ const APP = "http://eval-development-eval.10.10.1.183.xip.io";
 
 const lead = {
   username: "766458467",
-  Password: "****",
-  countrycode: "sr" 
+  Password: "wolf5",
+  countrycode: "+94",
+  clinic: "11;Åssiden Dyreklinikk;True"
 };
 
 let page;
 let browser;
+let codeSpan;
+let code;
+
 const width = 1920;
 const height = 1080;
-
 
 beforeAll(async () => {
   browser = await puppeteer.launch({
     headless: false,
-    slowMo: 80,
+    slowMo: 40,
+    devtools:false,
     args: [`--window-size=${width},${height}`]
   });
   page = await browser.newPage();
@@ -26,16 +30,38 @@ beforeAll(async () => {
 describe("Contact form", () => {
   test("user can submit a login request", async () => {
     await page.goto(APP);
+
+    // Login page
     await page.waitForSelector("#Username");
-    await page.click("select[name=CountryCode]");
-    await page.type("select[name=CountryCode]", lead.countrycode);
+    await page.select("select[name=CountryCode]", lead.countrycode);
     await page.click("input[name=Username]");
     await page.type("input[name=Username]", lead.username);
     await page.click("input[name=Password]");
     await page.type("input[name=Password]", lead.Password);
     await page.click("button[value=login]");
-    await page.waitFor(10000);
-    //await page.waitForSelector(".modal");
+
+    // Auth code page
+    await page.waitForSelector("input[name=CodeInput]");
+
+    // Extract the verification code from auth page
+    const featureArticle = (await page.$x("//span[contains(text(), 'Code')]"))[0];
+    const code = await page.evaluate(el => {
+        return String(el.textContent.split(':')[1].trim());
+    }, featureArticle);
+    
+    await page.click("input[name=CodeInput]");
+    await page.type("input[name=CodeInput]", code);
+    await page.click("button[value=login]");
+
+    // Select clinic page 
+    await page.waitForSelector("select[name=SelectedClinic]");
+    await page.select("select[name=SelectedClinic]", lead.clinic);
+    await page.click("button[value=login]");
+    
+    // Wait until appointments page loads
+    await page.waitForFunction('document.title == "Appointments | Vetserve"');
+
+    await page.waitFor(3000);
   }, 32000);
 });
 
